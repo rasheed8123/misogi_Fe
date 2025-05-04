8d88ae3256adf37ea772a4cae8956d558cbb9661
rash <abdulrasheed8123@gmail.com>
2025-05-04 05:32:08 +0530

    base url changed - updted the deployed backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'https://misogi-be.onrender.com/',
 });
 
 // Automatically attach token to every request
